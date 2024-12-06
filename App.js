0b3700fda8b951843bdc283e536e24493ed85965
AlbertoCarballo <jesusalbec_21@alu.uabcs.mx>
2024-12-05 21:48:44 -0700

    adicion de navegacion

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import LoginScreen from './components/login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginScreen />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="login">
+      <Stack.Screen name="login" component={LoginScreen} options={{}} />
+      {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Citas Médicas' }} /> */}
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
